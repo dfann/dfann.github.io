c11c3034b3529a1c774495b3a7680e4c98a66aec
MacBook <macbook@MacBooks-MacBook-Pro.local>
2020-06-03 06:57:27 -0400

    Updates

---
+++ precache-manifest.bbf036e76ff81ff4f0a6c61bb10cfdda.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e01c06ac085cd7732553fbbd6cbb4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d93f85df64c0a0087b32",
+    "url": "/static/css/main.7f0a3e0b.chunk.css"
+  },
+  {
+    "revision": "b254cffa05b732b057d5",
+    "url": "/static/js/2.5807ff4c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.5807ff4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d93f85df64c0a0087b32",
+    "url": "/static/js/main.fe7f8622.chunk.js"
+  },
+  {
+    "revision": "71437c1867da96b1eff1",
+    "url": "/static/js/runtime-main.ed394daa.js"
+  },
+  {
+    "revision": "da7d0e292beefd89978faed6b17b4da4",
+    "url": "/static/media/David-Fann.da7d0e29.jpeg"
+  },
+  {
+    "revision": "5e6e97aa477b581dc6319a472eb3a6f1",
+    "url": "/static/media/apple-camera-desk-office-med.5e6e97aa.jpg"
+  },
+  {
+    "revision": "29b636ee5c6b1bd43347a54ac7618be4",
+    "url": "/static/media/email.29b636ee.svg"
+  },
+  {
+    "revision": "d2873cddb45c789ab8a476c33beb05bd",
+    "url": "/static/media/github.d2873cdd.svg"
+  },
+  {
+    "revision": "0bc400df8c7677485cf21f4bb7cdf1f8",
+    "url": "/static/media/li.0bc400df.svg"
+  },
+  {
+    "revision": "773079255aaeed80ac3060bcea6b29c1",
+    "url": "/static/media/macbook-back.77307925.jpg"
+  },
+  {
+    "revision": "ffc6373e6fd77238e95d981ebfe9b5d9",
+    "url": "/static/media/macbook-three-quater.ffc6373e.jpg"
+  },
+  {
+    "revision": "15ea12cc2deab642faa59239e43ec951",
+    "url": "/static/media/pondu.15ea12cc.png"
+  },
+  {
+    "revision": "dce07831b1c66751c7af5cb9e47e7b84",
+    "url": "/static/media/thats-delicious.dce07831.png"
+  }
+]);
